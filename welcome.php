71d5b55c823e5a2c8672680c729e6e5b88b6853e
aradzx13 <160145325+aradzx13@users.noreply.github.com>
2024-12-09 12:24:14 +0330

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,53 @@
+<?php
+session_start(); // شروع سشن
+
+if (isset($_SESSION['message'])) {
+    echo "<div style='color: green;'>" . $_SESSION['message'] . "</div>";
+    unset($_SESSION['message']); // پاک کردن پیام بعد از نمایش
+}
+$servername = "localhost";
+$username = "root";
+$password = ""; // رمز عبور دیتابیس
+$dbname = "my_database"; // نام دیتابیس خود را اینجا وارد کنید
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $action = $_POST['action'];
+
+    if ($action === 'signup') {
+        // کد ثبت‌نام
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+        $sql = "INSERT INTO users (username, password) VALUES ('$username', '$hashed_password')";
+        if ($conn->query($sql) === TRUE) {
+            header("Location: welcome.php");
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    } elseif ($action === 'login') {
+		            echo "نام کاربری یا گذرواژه نادرست است.";
+
+        // کد ورود
+        $sql = "SELECT * FROM users WHERE username='$username'";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            if (password_verify($password, $row['password'])) {
+                header("Location: details.php");
+            } else {
+                echo "نام کاربری یا گذرواژه نادرست است.";
+            }
+        } else {
+            echo "نام کاربری یا گذرواژه نادرست است.";
+        }
+    }
+}
+
+$conn->close();
+?>
